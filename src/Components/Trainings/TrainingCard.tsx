39da11ee6fa80eca3c59c797e74687cb8f0f58b0
jhollinejepson <jhollinej@hexaware.com>
2025-06-26 14:41:37 +0530

    size fix

--- src/Components/Trainings/TrainingCard.tsx
+++ src/Components/Trainings/TrainingCard.tsx
@@ -18,7 +18,7 @@ export default function TrainingCard({ training, priority=false }: TrainingCardP
     <Card withBorder radius="md" shadow="sm" p="lg" bg={ priority===true ? 'linear-gradient(135deg, #8931b2   0%, #4f23c0 100%)' : "white"} style={{display:"flex", flexDirection:'column', justifyContent:'space-between', height: "200px" }}>
       <Stack gap="xs">
         <Group justify="space-between">
-          <Title order={5} c={ priority===true ? "white" : "black"}>{training.title}</Title>
+          <Title order={3} c={ priority===true ? "white" : "black"}>{training.title}</Title>
           <Badge variant="light" color={ priority===true ? "grape.1" : "grape.7"}>
             {training.level}
           </Badge>
